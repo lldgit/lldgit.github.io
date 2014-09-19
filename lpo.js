78fc6cff923e3f2feb4a10b39df91561f0cb14b3
lldgit <adrian.bechtold@legoland.de>
2014-09-19 14:40:41 +0200

    js

---
+++ lpo.js
@@ -0,0 +1,6 @@
+var preis_tageskarte_erwachsener_ab="";
+var preis_tageskarte_kind_ab="";
+var preis_familienkarte_4personen_ab="";
+var preis_familienkarte_5personen_ab="";
+
+//AB HIER NICHT MEHR EDITIEREN ODER ES MOEGEN DIE FINGER ABFALLEN
