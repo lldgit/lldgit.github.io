e95d347bbf0508e00892e30f3379d167836f360a
lldgit <adrian.bechtold@legoland.de>
2014-09-22 10:21:30 +0200

    js

--- lpo.js
+++ lpo.js
@@ -1,6 +1,46 @@
-var preis_tageskarte_erwachsener_ab="";
-var preis_tageskarte_kind_ab="";
-var preis_familienkarte_4personen_ab="";
-var preis_familienkarte_5personen_ab="";
+var preis_tageskarte_erwachsener_ab="27,00";
+var preis_tageskarte_kind_ab="30,38";
+var preis_familienkarte_4personen_ab="103,27";
+var preis_familienkarte_5personen_ab="104,27";
 
 //AB HIER NICHT MEHR EDITIEREN ODER ES MOEGEN DIE FINGER ABFALLEN
+
+var sidebarbox='<div class="lpo_mid lpo_red" style="width: 98%; margin: 0px; padding: 3px;">
+<h3>Bis zu 25% online sparen</h3>
+<ul>
+<li class="lpo_bigs"> 
+<ul class="lpo_bigs_second lpo_li">
+<li class="secondchild" style="width: 50%;">Kinder (3-11)</li>
+<li class="secondchild" style="width: 50%;">Erwachsene</li>
+</ul>
+<ul class="lpo_bigs_second lpo_li">
+<li class="secondchild" style="width: 50%;"><span>'+preis_tageskarte_kind_ab+' €</span></li>
+<li class="secondchild" style="width: 50%;"><span>'+preis_tageskarte_erwachsener_ab+' €</span></li>
+</ul>
+<ul class="lpo_bigs_second lpo_li">
+<li class="secondchild" style="width: 50%;">
+<div style="margin-top: -14px;"><a class="lpo_button lpo_white lpo_fr" id="lpolink1" href="https://secure.legoland.de/webapp/wcs/stores/servlet/ProductDisplay?langId=-3&amp;storeId=10663&amp;catalogId=24051&amp;categoryId=27101&amp;productId=176647&utm_source=sidebar&utm_medium=gutzuwissen_direct&utm_content=1k&utm_campaign=lpo">jetzt sichern</a></div>
+</li>
+<li class="secondchild" style="width: 50%;">
+<div style="margin-top: -14px;"><a class="lpo_button lpo_white lpo_fr" id="lpolink2" href="https://secure.legoland.de/webapp/wcs/stores/servlet/ProductDisplay?langId=-3&amp;storeId=10663&amp;catalogId=24051&amp;categoryId=27101&amp;productId=176647&utm_source=sidebar&utm_medium=gutzuwissen_direct&utm_content=1e&utm_campaign=lpo">jetzt sichern</a></div>
+</li>
+</ul>
+<ul class="lpo_bigs_second lpo_li">
+<li class="secondchild" style="width: 50%;">Familie 4 Pers.</li>
+<li class="secondchild" style="width: 50%;">Familie 5 Pers.</li>
+</ul>
+<ul class="lpo_bigs_second lpo_li">
+<li class="secondchild" style="width: 50%;"><span>'+preis_familienkarte_4personen_ab+' €</span></li>
+<li class="secondchild" style="width: 50%;"><span>'+preis_familienkarte_4personen_ab+' €</span></li>
+</ul>
+<ul class="lpo_bigs_second lpo_li" style="margin-top: -10px;">
+<li class="secondchild" style="width: 50%;">
+<div style="margin-top: -14px;"><a class="lpo_button lpo_white lpo_fr" id="lpolink3" href="https://secure.legoland.de/webapp/wcs/stores/servlet/ProductDisplay?langId=-3&amp;storeId=10663&amp;catalogId=24051&amp;categoryId=40601&amp;productId=176648&utm_source=sidebar&utm_medium=gutzuwissen_direct&utm_content=f4&utm_campaign=lpo">jetzt sichern</a></div>
+</li>
+<li class="secondchild" style="width: 50%;">
+<div style="margin-top: -14px;"><a class="lpo_button lpo_white lpo_fr" id="lpolink4" href="https://secure.legoland.de/webapp/wcs/stores/servlet/ProductDisplay?langId=-3&amp;storeId=10663&amp;catalogId=24051&amp;categoryId=40601&amp;productId=176649&utm_source=sidebar&utm_medium=gutzuwissen_direct&utm_content=f5&utm_campaign=lpo">jetzt sichern</a></div>
+</li>
+</ul>
+</li>
+</ul>
+</div>';
