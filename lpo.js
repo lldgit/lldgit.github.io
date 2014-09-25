f4c1db6048eeb41d68647aed8895b873994af9f7
lldgit <adrian.bechtold@legoland.de>
2014-09-25 10:41:29 +0200

    f

--- lpo.js
+++ lpo.js
@@ -4,18 +4,19 @@ var preis_familienkarte_4personen_ab="103,27";
 var preis_familienkarte_5personen_ab="104,27";
 
 //AB HIER NICHT MEHR EDITIEREN ODER ES MOEGEN DIE FINGER ABFALLEN
-
-
-if(Math.round(Math.random() * 1) + 1==1) {
+var str="Anreise";
+var url1=document.URL;
+if(url1.indexOf(str) != -1) {
+	var lpolink1="http://www.legoland.de/Jetzt-Tickets-sichern/";
+   var lpolink2="http://www.legoland.de/Jetzt-Tickets-sichern/";
+   var lpolink3="http://www.legoland.de/Jetzt-Tickets-sichern/"; 
+   var lpolink4="http://www.legoland.de/Jetzt-Tickets-sichern/"; 
+   var routenlink="1";
+}	else {
    var lpolink1="https://secure.legoland.de/webapp/wcs/stores/servlet/ProductDisplay?langId=-3&amp;storeId=10663&amp;catalogId=24051&amp;categoryId=27101&amp;productId=176647";
    var lpolink2="https://secure.legoland.de/webapp/wcs/stores/servlet/ProductDisplay?langId=-3&amp;storeId=10663&amp;catalogId=24051&amp;categoryId=27101&amp;productId=176647";
    var lpolink3="https://secure.legoland.de/webapp/wcs/stores/servlet/ProductDisplay?langId=-3&amp;storeId=10663&amp;catalogId=24051&amp;categoryId=40601&amp;productId=176648"; 
-   var lpolink4="https://secure.legoland.de/webapp/wcs/stores/servlet/ProductDisplay?langId=-3&amp;storeId=10663&amp;catalogId=24051&amp;categoryId=40601&amp;productId=176649";   
-} else {
-   var lpolink1="https://secure.legoland.de/webapp/wcs/stores/servlet/ProductDisplay?langId=-3&amp;storeId=10663&amp;catalogId=24051&amp;categoryId=27101&amp;productId=176647";
-   var lpolink2="https://secure.legoland.de/webapp/wcs/stores/servlet/ProductDisplay?langId=-3&amp;storeId=10663&amp;catalogId=24051&amp;categoryId=27101&amp;productId=176647";
-   var lpolink3="https://secure.legoland.de/webapp/wcs/stores/servlet/ProductDisplay?langId=-3&amp;storeId=10663&amp;catalogId=24051&amp;categoryId=40601&amp;productId=176648"; 
-   var lpolink4="https://secure.legoland.de/webapp/wcs/stores/servlet/ProductDisplay?langId=-3&amp;storeId=10663&amp;catalogId=24051&amp;categoryId=40601&amp;productId=176649";  
+   var lpolink4="https://secure.legoland.de/webapp/wcs/stores/servlet/ProductDisplay?langId=-3&amp;storeId=10663&amp;catalogId=24051&amp;categoryId=40601&amp;productId=176649"; 
 }
 
 
@@ -58,4 +59,18 @@ var sidebarbox='<div class=\"lpo_mid lpo_red\" style=\"width: 98%; margin: 0px; 
 '</li>'+
 '</ul>'+
 '</div>';
+if(routenlink=="1") {
+	var sidebarbox=sidebarbox +
+	'<div class=\"lpo_mid lpo_red\" style=\"width: 98%; margin: 0px; margin-top: 20px; padding: 3px; background:blue; font-size:13px;\">'+
+	'<h3 style=\"color:white; margin-top:5px; margin-bottom:5px;\">Route berechnen</h3>'+
+	'<form action=\"http://maps.google.com/maps\" method=\"get\" target=\"_blank\">'+
+  '<input class=\"inputbox\" style=\"width:98%;\" type=\"text\" name=\"saddr\" value=\"Startadresse\" onfocus=\"(this.value == \'Startadresse\') && (this.value = \'\')\"'+
+  '     onblur=\"(this.value == \'\') && (this.value = \'Startadresse\')\"  style=\"width:100%; border:1px solid lightgrey; font-size:20px; padding:3px;display:block; margin-bottom:5px;\" />'+
+  '<input type=\"hidden\" name=\"daddr\" value=\"LEGOLAND Deutschland, LEGOLAND Allee 2, 89312 G&uuml;nzburg, Deutschland\" />'+
+  '<input type=\"submit\" style=\"background:white; color:black; border:1px solid white; width:96%; font-size:17px; margin:5px;\" value=\"Karte aufrufen\" />'+
+'</form>'+
+
+			
+'			</div>';
+}
 $( "#lposalesboxsidebar" ).replaceWith(sidebarbox);
