87ce51dd68550504aa8ac9cb4a5da9084f5c877a
lldgit <adrian.bechtold@legoland.de>
2014-10-07 14:56:03 +0200

    Revert "Preise geändert"
    This reverts commit 05920db347e55efaa680b71a7c8971468f2eb561.

--- lpo.js
+++ lpo.js
@@ -1,7 +1,7 @@
 var preis_tageskarte_erwachsener_ab="30,38";
 var preis_tageskarte_kind_ab="27,00";
 var preis_familienkarte_4personen_ab="103,27";
-var preis_familienkarte_5personen_ab="105,27";
+var preis_familienkarte_5personen_ab="104,27";
 
 //AB HIER NICHT MEHR EDITIEREN ODER ES MOEGEN DIE FINGER ABFALLEN
 var str="Anreise";
