0cf721cbe592acca96b5f4f8b81a971deb9fdd24
lldgit <adrian.bechtold@legoland.de>
2014-10-07 15:02:50 +0200

    preistestwolle

--- lpo.js
+++ lpo.js
@@ -1,5 +1,5 @@
 var preis_tageskarte_erwachsener_ab="30,38";
-var preis_tageskarte_kind_ab="27,00";
+var preis_tageskarte_kind_ab="28,00";
 var preis_familienkarte_4personen_ab="103,27";
 var preis_familienkarte_5personen_ab="104,27";
 
