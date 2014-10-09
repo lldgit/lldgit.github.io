b2348db191767b02c5047444b923268381af4f75
lldgit <adrian.bechtold@legoland.de>
2014-10-09 14:14:13 +0200

    g

--- rumble.js
+++ rumble.js
@@ -1,7 +1,7 @@
 var str="/Entdeckt-LEGOLAND/Restaurant/";
 var url1=document.URL;
 if(url1.indexOf(str) != -1) {	
-	var rumble="<div id=\"funbox\"></div><a href=\"/\" id=\"rumblebumble\">test</a>";
+	var rumble="<div id=\"funbox\"></div><a href=\"/\" id=\"rumblebumble\">Jetzt <strong>25%</strong> bei Online-Tickets sparen!</a>";
 	$( "#funbox" ).replaceWith(rumble);
   $("#rumblebumble").delay( 800 ).animate({
     opacity:'0.5',
--- rumble.js~
+++ rumble.js~
@@ -4,7 +4,9 @@ if(url1.indexOf(str) != -1) {
 	var rumble="<div id=\"funbox\"></div><a href=\"/\" id=\"rumblebumble\">test</a>";
 	$( "#funbox" ).replaceWith(rumble);
   $("#rumblebumble").delay( 800 ).animate({
-    left:'250px',
     opacity:'0.5',
   });
+  $("#rumblebumble").delay( 1600 ).animate({
+    opacity:'1',
+  });  
 }	
