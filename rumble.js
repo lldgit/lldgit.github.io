ecaf8b064d0dda618cb88cce78d1e5a0f47ae3cb
lldgit <adrian.bechtold@legoland.de>
2014-10-09 10:45:03 +0200

    d

--- rumble.js
+++ rumble.js
@@ -3,6 +3,4 @@ var url1=document.URL;
 if(url1.indexOf(str) != -1) {	
 	var rumble="<div id=\"clouds\"></div><a href=\"/\" id=\"rumblebumble\">test</a>";
 	$( "#clouds" ).replaceWith(rumble);
-	$('#rumblebumble').jrumble();
-	$('#rumblebumble').trigger('startRumble');
 }	
