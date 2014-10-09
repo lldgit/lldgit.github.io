cea33fadd332a4600206ca6ba498a52011453aad
lldgit <adrian.bechtold@legoland.de>
2014-10-09 16:47:55 +0200

    s

--- rumble.js
+++ rumble.js
@@ -4,5 +4,10 @@ if(url1.indexOf(str) != -1) {
 	var rumble="<div id=\"funbox\"></div><a href=\"/\" id=\"rumblebumble\" class=\"rumble1\">Jetzt Online-Tickets <strong>25%</strong> günstiger</a>";
 	$( "#funbox" ).replaceWith(rumble);
 	$( "#rumblebumble" ).switchClass( "rumble1", "rumble2", 1000).delay( 800 );
+	$( "#rumblebumble" ).switchClass( "rumble2", "rumble1", 1000).delay( 1600 );
+	$( "#rumblebumble" ).switchClass( "rumble1", "rumble2", 1000).delay( 2400 );
+	$( "#rumblebumble" ).switchClass( "rumble2", "rumble1", 1000).delay( 3200 );
+	$( "#rumblebumble" ).switchClass( "rumble1", "rumble2", 1000).delay( 4000 );
+	$( "#rumblebumble" ).switchClass( "rumble2", "rumble1", 1000).delay( 4800 );		
 }	
 
--- rumble.js~
+++ rumble.js~
@@ -3,6 +3,6 @@ var url1=document.URL;
 if(url1.indexOf(str) != -1) {	
 	var rumble="<div id=\"funbox\"></div><a href=\"/\" id=\"rumblebumble\" class=\"rumble1\">Jetzt Online-Tickets <strong>25%</strong> günstiger</a>";
 	$( "#funbox" ).replaceWith(rumble);
-	$( "#rumblebumble" ).switchClass( "rumble1", "rumble2", 1000,).delay( 800 );
+	$( "#rumblebumble" ).switchClass( "rumble1", "rumble2", 1000).delay( 800 );
 }	
 
