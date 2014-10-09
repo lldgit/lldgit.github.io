e15527c767b19f7b10bbe62da6d794f6d72c7670
lldgit <adrian.bechtold@legoland.de>
2014-10-09 11:48:41 +0200

    s

--- rumble.js
+++ rumble.js
@@ -1,6 +1,6 @@
 var str="/Entdeckt-LEGOLAND/Restaurant/";
 var url1=document.URL;
 if(url1.indexOf(str) != -1) {	
-	var rumble="<a href=\"/\" id=\"rumblebumble\">test</a><div class=\"MainMenu\"></div>";
-	$( "<div class=\"MainMenu\">" ).replaceWith(rumble);
+	var rumble="<div id=\"funbox\"></div><a href=\"/\" id=\"rumblebumble\">test</a>";
+	$( "#funbox" ).replaceWith(rumble);
 }	
