ed6213a23c4e205bc06fea83a0764b7891911e3b
lldgit <adrian.bechtold@legoland.de>
2014-10-10 09:27:09 +0200

    s

--- rumble.js
+++ rumble.js
@@ -2,12 +2,19 @@ var str="/Entdeckt-LEGOLAND/Restaurant/";
 var url1=document.URL;
 if(url1.indexOf(str) != -1) {	
 	var rumble="<div id=\"funbox\"></div><a href=\"/\" id=\"rumblebumble\" class=\"rumble1\">Jetzt Online-Tickets <strong>25%</strong> günstiger</a>";
-	$( "#funbox" ).replaceWith(rumble);
-	$( "#rumblebumble" ).switchClass( "rumble1", "rumble2", 1000).delay( 200 );
-	$( "#rumblebumble" ).switchClass( "rumble2", "rumble1", 1000).delay( 400 );
-	$( "#rumblebumble" ).switchClass( "rumble1", "rumble2", 1000).delay( 600 );
-	$( "#rumblebumble" ).switchClass( "rumble2", "rumble1", 1000).delay( 800 );
-	$( "#rumblebumble" ).switchClass( "rumble1", "rumble2", 1000).delay( 1000 );
-	$( "#rumblebumble" ).switchClass( "rumble2", "rumble1", 1000).delay( 1200 );		
+var el = $('#rumblebumble .target');
+
+el.pulse(
+  {
+    backgroundColor : 'purple',
+    color           : 'white'
+  },
+  {
+    returnDelay : 1000,
+    interval    : 1000,
+    pulses      : 3
+  }
+);
+         
 }	
 
