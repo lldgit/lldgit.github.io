0cf72cac9f95b460176c4b48065b7c2805aca10d
lldgit <adrian.bechtold@legoland.de>
2014-10-10 09:33:47 +0200

    s

--- rumble.js
+++ rumble.js
@@ -3,7 +3,6 @@ var url1=document.URL;
 if(url1.indexOf(str) != -1) {	
 	var rumble="<div id=\"funbox\"></div><a href=\"/\" id=\"rumblebumble\" class=\"rumble1\">Jetzt Online-Tickets <strong>25%</strong> günstiger</a>";
 	$( "#funbox" ).replaceWith(rumble);
-var el = $('#rumblebumble .target');
 
 
 
