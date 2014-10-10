0ef3c635bd1fe23ea0ce70cca0a38736280dc744
lldgit <adrian.bechtold@legoland.de>
2014-10-10 10:08:05 +0200

    s

--- rumble.js
+++ rumble.js
@@ -3,13 +3,11 @@ var url1=document.URL;
 if(url1.indexOf(str) != -1) {	
 	var rumble="<div id=\"funbox\"></div><a href=\"/\" id=\"rumblebumble\" class=\"rumble1\">Jetzt Online-Tickets <strong>25%</strong> günstiger</a>";
 	$( "#funbox" ).replaceWith(rumble);
-	           var properties = {
-               color : 'red'
-            };
-
-            var el = $('#rumblebumble .target');
-
-            el.pulse(properties, { pulses : 2 });
+    for (var i = 0; i < 3; i++ ) {
+        $("#rumblebumble")
+            .animate( { backgroundColor: "blue" }, 2000 )
+            .animate( { backgroundColor: "red" }, 2000 );
+    }
          
 
 }	
