874cfe27e718d1094cd640f9b5fde9e7afb57d3d
lldgit <adrian.bechtold@legoland.de>
2014-10-14 12:48:24 +0200

    s

--- rumble.js
+++ rumble.js
@@ -19,7 +19,6 @@ if(url1.indexOf(str) != -1) {
 if(url1=="http://www.legoland.de") {
 	var finderror=2;
 }
-alert(document.url)
 var str="/en/";
 var url1=document.URL;
 if(url1.indexOf(str) == -1 && finderror==1) {	
