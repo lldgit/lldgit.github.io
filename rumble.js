d5fc00f554b38583b6e394bba9fc2a03470843c8
lldgit <adrian.bechtold@legoland.de>
2014-11-06 10:46:27 +0100

    s

--- rumble.js
+++ rumble.js
@@ -59,7 +59,7 @@ if(url1=="http://www.legoland.de/") {
 }
 var str="/en/";
 var url1=document.URL;
-if(url1.indexOf(str) == -1 && finderror==1 && browsererror==1) {	
+
 	var rumble="<div id=\"funbox\"></div><a href=\"/Jetzt-Tickets-sichern/\" id=\"rumblebumble\" style=\"background-color:#da291c;\" onClick=\"_gaq.push([\'_trackEvent\', \'Stoerer\', \'Navi\', \'Online-Tickets\']);\">Online-Tickets</a>";
 	$( "#funbox" ).replaceWith(rumble);
     for (var i = 0; i < 3; i++ ) {
@@ -73,11 +73,11 @@ if(url1.indexOf(str) == -1 && finderror==1 && browsererror==1) {
             
     }
 
-}
+
 
 var finderror=2;
-if(url1.indexOf(str) == -1 && finderror==1) {	
+
      var rumble2="<a href=\"/Jetzt-Tickets-sichern/\" class=\"topstoerer\" onClick=\"_gaq.push([\'_trackEvent\', \'Stoerer\', \'Content\', \'FamilienticketsHalloween\']);\">Für kurze Zeit: Tageskarte nur 25 &euro; und gruselige Halloween-Wochen erleben &raquo;</a><div class=\"c\"></div>";
 $( ".c:first" ).replaceWith(rumble2);
-}
+
 
