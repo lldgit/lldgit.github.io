d6d9b519cb1d6d978f688962ff09fcc98cbad70a
lldgit <adrian.bechtold@legoland.de>
2014-11-11 13:49:37 +0100

    s

--- rumble.js
+++ rumble.js
@@ -2,7 +2,7 @@
 var str ="Piraten-Bowling";
 var url1=document.URL;
 if (url1.indexOf(str) != -1) {
-	$('#sidebar').animate({ marginTop: '50px'}, 1000);
+	$('#sidebar').$('#maincontent').animate({ marginTop: '50px'}, 1000);
 }
 
 
