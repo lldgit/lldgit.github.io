18a2e3ff3bcd2b7c47e8830a49d99ea9a6660489
lldgit <adrian.bechtold@legoland.de>
2014-11-12 14:16:23 +0100

    d

--- rumble.js
+++ rumble.js
@@ -165,7 +165,25 @@ if($.cookie('lldbig2') === undefined || $.cookie('lldbig2') === null) {
 	
 	}
 	
-
+function addEvent(obj, evt, fn) {
+    if (obj.addEventListener) {
+        obj.addEventListener(evt, fn, false);
+    }
+    else if (obj.attachEvent) {
+        obj.attachEvent("on" + evt, fn);
+    }
+}
+addEvent(window,"load",function(e) {
+    addEvent(document, "mouseout", function(e) {
+        e = e ? e : window.event;
+        var from = e.relatedTarget || e.toElement;
+        if (!from || from.nodeName == "HTML") {
+            // stop your drag event here
+            // for now we can just use an alert
+            alert("left window");
+        }
+    });
+});
 
 
 
