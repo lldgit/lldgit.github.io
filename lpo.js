96c36c7b22eb299dc5df7d5684b6d6d3319922a5
lldgit <adrian.bechtold@legoland.de>
2014-12-23 14:42:12 +0100

    sdfg

--- lpo.js
+++ lpo.js
@@ -21,7 +21,7 @@ if(url1.indexOf(str) != -1) {
 
 
 var sidebarbox='<div class=\"lpo_mid lpo_red\" style=\"width: 98%; margin: 0px; padding: 3px;\">'+
-'<h3>Tickets ab 24 &euro; pro Person!</h3>'+
+'<h3>Tickets ab 26 &euro; pro Person!</h3>'+
 '<ul>'+
 '<li class=\"lpo_bigs\"> '+
 '<ul class=\"lpo_bigs_second lpo_li\">'+
