67753da2467e8f4dc7088cc0a4af6df4c02073e9
lldgit <adrian.bechtold@legoland.de>
2015-01-29 14:12:10 +0100

    d

--- rumble.js
+++ rumble.js
@@ -14,13 +14,11 @@ var endDate = "February 11, 2015 09:30:00";
 
 
 
-if($.cookie('cookiestor')=! "nogo") {
-	     var rumble5="<div id=\"cookieding\" style=\"height:9px; background:#a7dff6; font-size:8px; width:100%; text-align:center;\">Um Ihnen bestmöglichen Service zu garantieren verwendet unsere Webseite Cookies. Klicken Sie <a href=\"\/Ueber-LEGOLAND\/Cookies\/\">hier</a> um mehr zu erfahren. <a href=\"\">x<\/a><\/div>";
+
+
+     var rumble5="<div id=\"cookieding\" style=\"height:9px; background:#a7dff6; font-size:8px; width:100%; text-align:center;\">Um Ihnen bestmöglichen Service zu garantieren verwendet unsere Webseite Cookies. Klicken Sie <a href=\"\/Ueber-LEGOLAND\/Cookies\/\">hier</a> um mehr zu erfahren. <a href=\"\">x<\/a><\/div>";
 $( "#background-center" ).replaceWith(rumble5);
 
-
-
-}
 
 
 
