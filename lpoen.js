1763ab18b1f7b75bb877ec75631028d2630b2276
lldgit <adrian.bechtold@legoland.de>
2015-04-07 10:34:05 +0200

    d

--- lpoen.js
+++ lpoen.js
@@ -1,7 +1,7 @@
-var preis_tageskarte_erwachsener_ab="31.12";
-var preis_tageskarte_kind_ab="27.75";
-var preis_familienkarte_4personen_ab="25.00";
-var preis_familienkarte_5personen_ab="25.00";
+var preis_tageskarte_erwachsener_ab="31,12";
+var preis_tageskarte_kind_ab="27,75";
+var preis_familienkarte_4personen_ab="26,50";
+var preis_familienkarte_5personen_ab="26,19";
    var lpolink1="https://secure.legoland.de/webapp/wcs/stores/servlet/ProductDisplay?storeId=10663&langId=-1&catalogId=24051&categoryId=27101&productId=176647";
    var lpolink2="https://secure.legoland.de/webapp/wcs/stores/servlet/ProductDisplay?storeId=10663&langId=-1&catalogId=24051&categoryId=27101&productId=176647";
    var lpolink3="https://secure.legoland.de/webapp/wcs/stores/servlet/ProductDisplay?storeId=10663&langId=-1&catalogId=24051&categoryId=40601&productId=176648"; 
@@ -19,7 +19,7 @@ if(url1.indexOf(str) != -1) {
 
 
 var sidebarbox='<div class=\"lpo_mid lpo_red\" style=\"width: 98%; margin: 0px; padding: 3px;\">'+
-'<h3>Tickets from 25 Euro p.p. </h3>'+
+'<h3>Save up to 25% </h3>'+
 '<ul>'+
 '<li class=\"lpo_bigs\"> '+
 '<ul class=\"lpo_bigs_second lpo_li\">'+
